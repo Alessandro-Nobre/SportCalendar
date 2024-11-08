eb7aa91a725131a4de1a3b39ad0c0e4d1154682e
Alessandro-Nobre <alex.ousa99@gmail.com>
2024-11-08 14:09:47 +0000

    first commit

---
+++ src/test/java/com/sportradar/SportCalendar/SportCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportradar.SportCalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
