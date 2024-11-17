5e9f16631e15c3cd280a061b5932c267dee5c509
Alessandro-Nobre <alex.ousa99@gmail.com>
2024-11-17 21:20:56 +0000

    Inicializade project database with some data

--- src/main/java/com/sportradar/SportCalendar/repository/TeamRepository.java
+++ src/main/java/com/sportradar/SportCalendar/repository/TeamRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TeamRepository extends JpaRepository<TeamEntity, Integer> {
-    TeamEntity getByTeamId(int teamId);
 }
