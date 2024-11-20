600d919929641f65a5b4456eac436d309a27e44b
Alessandro-Nobre <alex.ousa99@gmail.com>
2024-11-20 14:18:21 +0000

    Optimization of code, move index.html and afterRequest.html out of menu folder, adjustment of color and implementation of FrontEndController

---
+++ src/main/java/com/sportradar/SportCalendar/controller/FrontEndController.java
@@ -0,0 +1,13 @@
+package com.sportradar.SportCalendar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontEndController {
+
+    @GetMapping("/")
+    public String index() {
+        return "forward:/menu/index.html";
+    }
+}
